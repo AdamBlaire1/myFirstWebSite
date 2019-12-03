eee35c829413af70ba30d4cab7aeffff07ed725f
AdamBlaire1 <Adam.Blaire@gmail.com>
2019-12-03 09:48:24 -0800

    Frist commit

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var size = $("a").css("fontSize");
+    $("a").hover(function() {
+        $(this).css("fontSize", "30px", );
+    }, function() {
+        $(this).css("fontSize", size);
+    });
+
+    loadDoc()
+});
+
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            gotRepos(this.responseText)
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/AdamBlaire1/repos", true);
+    xhttp.send();
+}
+
+
+
+function gotRepos(raw) {
+    //$('#raw-GitHub').text(raw)
+
+
+
+    const data = JSON.parse(raw)
+
+    // do some stuff with `data`
+
+    for (var repo of data) {
+        $('<p>').text(`My repositories! : ${repo.name}!`).appendTo('#GitHub')
+    }
+}
